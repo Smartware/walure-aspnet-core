44210a954c84dc3350b336f07aa34c203f3a71f0
Prolifik Lexzy <pro_lifik@live.com>
2021-11-17 14:27:52 +0100

    Add project files.

---
+++ WebApp/Controllers/ArithmeticController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class ArithmeticController : Controller
+    {
+        public string Multiply()
+        {
+            int result = 8 * 7;
+            return $"8 X 7 = {result}";
+        }
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public int Add()
+        {
+            return (5 * 4);
+        }
+
+        public string Welcome()
+        {
+            return "Welcome Ejiro";
+        }
+
+        public string Index()
+        {
+            return "Welcome to asp.net core";
+        }
+    }
+}
