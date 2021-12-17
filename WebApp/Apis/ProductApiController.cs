a27367cf0cd37450e6cb414ab1d1ad8a3bbbb6e5
Prolifik Lexzy <pro_lifik@live.com>
2021-12-17 13:16:53 +0100

    CRUD for API

--- WebApp/Apis/ProductApiController.cs
+++ WebApp/Apis/ProductApiController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApp.Context;
 using WebApp.Models;
 
 namespace WebApp.Apis
@@ -11,59 +12,45 @@ namespace WebApp.Apis
     [Route("api/[controller]")]
     public class ProductApiController : Controller
     {
+        private readonly WebDbContext _context;
+        public ProductApiController(WebDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpDelete("delete-product")]
+        public IActionResult DeleteProduct(int id)
+        {
+            return Ok(true);
+        }
+
+        [HttpPut("update-product")]
+        public IActionResult UpdateProduct(Product product)
+        {
+            return Ok(product);
+        }
+
         [HttpGet("get-details/{id}")]
         public IActionResult GetDetails(int id)
         {
-            var productList = new List<Product>();
-
-            productList.Add(new Product
-            {
-                Id = 1,
-                Name = "Tin Tomato"
-            });
-            productList.Add(new Product
-            {
-                Id = 2,
-                Name = "Tissue Paper"
-            });
-            productList.Add(new Product
-            {
-                Id = 3,
-                Name = "Cutleries"
-            });
-
-            var findProduct = productList.FirstOrDefault(p => p.Id == id);
+            var findProduct = _context.Products.FirstOrDefault(p => p.Id == id);
             return Ok(findProduct);
         }
         [HttpPost("create-product")]
         public IActionResult CreateProduct([FromBody] Product product)
         {
-            product.Id = 1;
-            product.Price = 800;
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
             return Ok(product);
         }
 
         [HttpGet("get-products")]
         public IActionResult GetProducts()
         {
-            var productList = new List<Product>();
-
-            productList.Add(new Product
-            {
-                Id = 1,
-                Name = "Tin Tomato"
-            });
-            productList.Add(new Product
-            {
-                Id = 2,
-                Name = "Tissue Paper"
-            });
-            productList.Add(new Product
-            {
-                Id = 3,
-                Name = "Cutleries"
-            });
-
+            var productList = _context.Products.ToList();
             return Ok(productList);
         }
     }
---
+++ WebApp/Context/WebDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Context
+{
+    public class WebDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
